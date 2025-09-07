059a8d357da9f6331da11fe96ba35c6f4e437326
allen-li1231 <supermrli@hotmail.com>
2025-09-07 18:05:38 -0400

    fix 'index out of bound' error when adding multiple cells in a short time

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -145,17 +145,19 @@ export async function activate(context: vscode.ExtensionContext) {
 				if (cellAdded?.metadata.id !== undefined
 					&& cellAdded.metadata.id === cellRemoved?.metadata.id) {
 					logDebug("onDidChangeNotebookDocument: cellReplaced", cellAdded);
-					kernel.registerParagraphUpdate(cellAdded);
+					kernel.updateParagraph(cellAdded);
 				}
 				else {
-					// normal add/remove cell registering
+					// normal add/remove cell registeration
 					if (cellAdded !== undefined) {
-						logDebug("onDidChangeNotebookDocument: cellAdded", cellAdded);
-						kernel.registerParagraphUpdate(cellAdded);
+						logDebug("onDidChangeNotebookDocument: cellAdded", cellAdded.index);
+						// update right away,
+						// otherwise more added cell contaminate the indices
+						kernel.updateParagraph(cellAdded);
 					}
 					if (cellRemoved !== undefined) {
 						logDebug("onDidChangeNotebookDocument: cellRemoved", cellRemoved);
-						kernel.registerParagraphUpdate(cellRemoved);
+						kernel.updateParagraph(cellRemoved);
 					}
 				}
 			}
